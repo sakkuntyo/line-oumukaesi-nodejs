b2c10797d5bc473b82eb0652eebb6b13eb486627
sakkuntyo <d-sasaki@know-net.co.jp>
2019-08-04 02:11:26 +0900

    おうむ返し実装

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
+var line    = require('@line/bot-sdk');
+var client  = new line.Client({
+  channelAccessToken: '<AccessToken>'
+});
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/line', function(req, res, next) {
+  res.status(200).end;
+  res.json({ webhook: 'called!' })
+
+  client.replyMessage(req.body.events[0].replyToken,{
+    type: 'text',
+    text: req.body.events[0].message.text
+  })
+    .then(() => {
+      console.log("success");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
